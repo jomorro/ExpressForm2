5c799c4ef9f4e90831fa632e2e5586f48b7271e4
jomorro <41160359+jomorro@users.noreply.github.com>
2018-10-31 10:22:00 -0400

    orginal file upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+const publicFolderPath = path.join(__dirname, "public")
+
+app.use(express.json())
+app.use(express.static(publicFolderPath))
+
+const users = []
+
+app.post('/api/user/', function(request, response) {
+    if (users.map(userObject => userObject.username).some(username => username == request.body.username)) {
+        response.status(409).send({error: 'username already in use'});
+    } else {
+        const user = request.body;
+        user.id = Math.floor(Math.random() * 1000000000);
+        users.push(user);
+        response.status(201).send(user);
+    }
+});
+
+app.listen(3000);
